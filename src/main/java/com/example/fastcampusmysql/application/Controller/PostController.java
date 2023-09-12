156bb859849ff2b72839918144ab4cabee7357e8
ldhsa <ccmchanggo@gmail.com>
2023-09-12 21:21:27 +0900

    최초

--- src/main/java/com/example/fastcampusmysql/application/Controller/PostController.java
+++ src/main/java/com/example/fastcampusmysql/application/Controller/PostController.java
@@ -4,9 +4,13 @@ package com.example.fastcampusmysql.application.Controller;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
 import com.example.fastcampusmysql.domain.post.dto.PostCommand;
+import com.example.fastcampusmysql.domain.post.entitiy.Post;
 import com.example.fastcampusmysql.domain.post.service.PostReadService;
 import com.example.fastcampusmysql.domain.post.service.PostWriteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,5 +31,15 @@ public class PostController {
     public List<DailyPostCount> getDailyPostCounts( @RequestBody DailyPostCountRequest request){
         return postReadService.getDailyPostCounts(request);
     }
+
+    @PostMapping("/member/{memberId}")
+    public Page<Post> getPosts(
+            @PathVariable Long memberId,
+            Pageable pageable
+
+    ){
+        return postReadService.getPosts(memberId, pageable);
+    }
+
 
 }
--- src/main/java/com/example/fastcampusmysql/domain/post/service/PostReadService.java
+++ src/main/java/com/example/fastcampusmysql/domain/post/service/PostReadService.java
@@ -2,8 +2,12 @@ package com.example.fastcampusmysql.domain.post.service;
 
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
 import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
+import com.example.fastcampusmysql.domain.post.entitiy.Post;
 import com.example.fastcampusmysql.domain.post.respository.PostRepsitory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 
@@ -23,4 +27,11 @@ public class PostReadService {
         */
     return postRepsitory.groupBycreatedDate(request);
     }
+
+    public Page<Post> getPosts(Long memberId, Pageable pageRequest){
+        return postRepsitory.findAllByMemberId(memberId, pageRequest);
+
+    }
+
+
 }
